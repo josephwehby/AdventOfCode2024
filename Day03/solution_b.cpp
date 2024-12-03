31a831bdac5282295b992fd07b5c4cf4cff597f7
josephwehby <josephw31@hotmail.com>
2024-12-03 17:52:55 -0600

    finished day 3

--- Day03/solution_b.cpp
+++ Day03/solution_b.cpp
@@ -2,20 +2,36 @@
 #include <regex>
 
 int main() {
-  std::regex reg("mul\\(([0-9]+),([0-9]+)\\)");
+  std::regex reg("do\\(\\)|don't\\(\\)|mul\\(([0-9]+),([0-9]+)\\)");
   
   std::string line;
   long long total = 0;
+  bool dont = false;
+
   while(getline(std::cin, line)) {
     std::smatch m;
     std::sregex_iterator begin(line.begin(), line.end(), reg);
     std::sregex_iterator end;
-
+    
     for (auto it = begin; it != end; it++) {
-      std::smatch match = *it;
-      int first = std::stoi(match[1].str());
-      int second = std::stoi(match[2].str());
-      total += (first*second); 
+      std::string m = it->str();
+      if (m == "do()") {
+        dont = false;
+        continue;
+      }
+
+      if (m == "don't()") {
+        dont = true;
+        continue;
+      }
+      
+      if (!dont) {
+        std::smatch match = *it;
+        int first = std::stoi(match[1].str());
+        int second = std::stoi(match[2].str());
+        total += (first*second); 
+      }
+     
     }
   }
 
